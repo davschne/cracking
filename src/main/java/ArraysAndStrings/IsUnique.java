95d9e8954c936c4e85ef08eaf4f1f32fdf6f9c94
davschne <davschne@gmail.com>
2016-03-18 16:24:49 -0700

    1.1: Is Unique

--- src/main/java/ArraysAndStrings/IsUnique.java
+++ src/main/java/ArraysAndStrings/IsUnique.java
@@ -1,7 +1,29 @@
-package laakmann;
+import java.util.HashMap;
+import java.util.Scanner;
 
 class IsUnique {
   public static boolean isUnique(String string) {
+    // create hash table
+    // capacity based on char type
+    // value of true means character is present in the string
+    HashMap<Character, Boolean> hash = new HashMap<>(256);
+    for (int i = 0; i < string.length(); i++) {
+      char c = string.charAt(i);
+      if (hash.containsKey(c)) {
+        return false;
+      }
+      else {
+        hash.put(c, true);
+      }
+    }
+    return true;
+  }
 
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    while (scanner.hasNext()) {
+      String input = scanner.next();
+      System.out.println(IsUnique.isUnique(input) ? "true" : "false");
+    }
   }
 }
