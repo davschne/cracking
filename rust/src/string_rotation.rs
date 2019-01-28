b7d724cf734a56d2b7ba6d108fdc309a0625ae94
davschne <davschne@gmail.com>
2019-01-27 17:44:38 -0800

    1.9: handle case of unequal-length strings

--- rust/src/string_rotation.rs
+++ rust/src/string_rotation.rs
@@ -2,7 +2,7 @@
  * 1.9 String Rotation: Assume you have a method `isSubstring` which checks if one word is a substring of another. Given two strings, `s1` and `s2`, write code to check if `s2` is a rotation of `s1` using only one call to `isSubstring` (e.g. "waterbottle" is a rotation of "erbottlewat").
  */
 pub fn is_rotation(s1: &str, s2: &str) -> bool {
-    is_substring(&s1.repeat(2), s2)
+    s1.len() == s2.len() && is_substring(&s1.repeat(2), s2)
 }
 
 fn is_substring(haystack: &str, needle: &str) -> bool {
@@ -16,5 +16,6 @@ mod tests {
     pub fn test_is_rotation() {
         assert_eq!(is_rotation("waterbottle", "erbottlewat"), true);
         assert_eq!(is_rotation("waterbottle", "rebottlewat"), false);
+        assert_eq!(is_rotation("hello", "hellohello"), false);
     }
 }
